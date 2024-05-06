554d69e54e1361989e765343823c4ba516872e81
Sabbir Hasan <sabbirh1000@gmail.com>
2024-05-06 19:48:42 +0600

    Implement 16*16 grid square box with flexbox

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+
+const body = document.querySelector("body");
+body.style.boxSizing = "border-box";
+
 const mainContainer = document.querySelector(".main-container");
 
 //button
@@ -8,10 +12,10 @@ button.style.height = "50px";
 
 //grid container
 const div = document.createElement("div");
-div.classList.add = "gridContainer";
-div.style.border = "2px solid black";
-div.style.width = "650px";
-div.style.height = "650px";
+div.style.border = "1px solid black";
+div.style.width = "960px";
+div.style.height = "960px";
+div.classList.add("grid-container");
 
 //main container
 mainContainer.style.display = "flex";
@@ -22,6 +26,21 @@ mainContainer.style.alignItems = "center";
 mainContainer.appendChild(button);
 mainContainer.appendChild(div);
 
+//grid container
+const gridContainer = document.querySelector(".grid-container");
+gridContainer.style.display = "flex";
+gridContainer.style.flexWrap= "wrap";
 
+
+for(let i = 0; i < 16 * 16; i++) {
+
+    const grid = document.createElement("div");
+    grid.classList.add("grid");
+    grid.style.display = "flex";
+    grid.style.width = "58.666666px";
+    grid.style.height = "58.66666px";
+    grid.style.border = "1px solid black";
+    gridContainer.append(grid);
+}
 
 
