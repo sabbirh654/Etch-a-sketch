cd4b6e0aeda7b0a737d334c1cded32a8923226ba
Sabbir Hasan <sabbirh1000@gmail.com>
2024-05-06 21:56:20 +0600

    Add button event and hovering event

--- app.js
+++ app.js
@@ -2,6 +2,9 @@
 const body = document.querySelector("body");
 body.style.boxSizing = "border-box";
 
+const GRID_CONTAINER_WIDTH = 960;
+const GRID_CONTAINER_HEIGHT = 960; 
+
 const mainContainer = document.querySelector(".main-container");
 
 //button
@@ -12,9 +15,9 @@ button.style.height = "50px";
 
 //grid container
 const div = document.createElement("div");
-div.style.border = "1px solid black";
-div.style.width = "960px";
-div.style.height = "960px";
+div.style.outline = "1px solid black";
+div.style.width = GRID_CONTAINER_WIDTH + 'px';
+div.style.height = GRID_CONTAINER_HEIGHT + 'px';
 div.classList.add("grid-container");
 
 //main container
@@ -31,16 +34,65 @@ const gridContainer = document.querySelector(".grid-container");
 gridContainer.style.display = "flex";
 gridContainer.style.flexWrap= "wrap";
 
+const mouseEnterEventHandler = function(e) {
+    e.target.style.backgroundColor = "black";
+}
+
 
 for(let i = 0; i < 16 * 16; i++) {
 
     const grid = document.createElement("div");
     grid.classList.add("grid");
     grid.style.display = "flex";
-    grid.style.width = "58.666666px";
-    grid.style.height = "58.66666px";
-    grid.style.border = "1px solid black";
+    grid.style.width = "60px";
+    grid.style.height = "60px";
+    grid.style.outline = "1px solid black";
     gridContainer.append(grid);
+
+    grid.addEventListener('mouseenter', mouseEnterEventHandler);
 }
+
+const buttonEventHandler = function() {
+
+    const input = GetUserInput();
+
+    if(input === undefined) {
+        return;
+    }
+
+    let squareBoxWidth = (GRID_CONTAINER_WIDTH / input) + 'px';
+    let squareBoxHeight = squareBoxWidth;
+
+    gridContainer.innerHTML = "";
+
+    for(let i = 0; i < input * input; i++) {
+
+        const grid = document.createElement("div");
+        grid.classList.add("grid");
+        grid.style.width = squareBoxWidth
+        grid.style.height = squareBoxHeight;
+        grid.style.outline = "1px solid black";
+        gridContainer.append(grid);
+
+        grid.addEventListener('mouseenter', mouseEnterEventHandler);
+    }
+}
+
+function GetUserInput() {
+    const value = (prompt("Enter a value : "));
+
+    if(value === null) {
+        return;
+    }
+
+    if(value === null || isNaN(value) || value < 0 || value > 100) {
+        GetUserInput();
+    }
+
+    return +value;
+}
+
+const btn = document.querySelector("button");
+btn.addEventListener('click', buttonEventHandler);
 
 
